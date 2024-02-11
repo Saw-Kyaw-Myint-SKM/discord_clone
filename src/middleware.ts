1451f089b1231b1ea378fc035e571f4619eb84dd
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-02-11 22:02:29 +0630

    add data base

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 // export function middleware(request: NextRequest) {
 //   return NextResponse.redirect(new URL('/', request.url))
 // }
-export default authMiddleware({});
+
+export default authMiddleware({
+  publicRoutes: ["/api/uploadthing"],
+});
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };

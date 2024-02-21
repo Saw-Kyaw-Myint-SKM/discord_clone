9c42d2f51cc85e152fe7d1ebb00b14a25e43084d
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-02-21 09:07:42 +0630

    add generate invite code

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -14,6 +15,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 };
--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -9,14 +9,24 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
+import { useModal } from "../../../hooks/use-modal-store";
 
 interface ServerHeaderProps {
-  server: ServerWithMembersWithProfiles | null;
+  server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
   return (
@@ -36,6 +46,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       >
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", {server})}
             className="text-indigo-600 dark:text-indigo-400 
           px-3 py-2 text-sm cursor-pointer"
           >
@@ -61,9 +72,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <PlusCircle className="h-4 w-43 ml-auto" />
           </DropdownMenuItem>
         )}
-        {isModerator && (
-            <DropdownMenuSeparator />
-        )}
+        {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
           <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
             Delete Server

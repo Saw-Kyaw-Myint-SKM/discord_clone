625ea4b2f6c97f959ce5f783d2f14f7e9f5ca324
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-03-09 16:03:24 +0630

    delete channel

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
 export type ModalType =
@@ -6,13 +6,15 @@ export type ModalType =
   | "invite"
   | "editServer"
   | "members"
-  | "createChannel" 
-  | "leaveServer" 
-  | "deleteServer";
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer"
+  | "deleteChannel";
 
 interface ModalData {
   server?: Server;
-  channelType?:ChannelType
+  channel?: Channel;
+  channelType?: ChannelType;
 }
 interface ModalStore {
   type: ModalType | null;
